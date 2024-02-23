0ceff08ff238b3995a1611f5d7d72e8b0c384ff0
cigdemin <cigmenci@hotmail.com>
2024-02-23 12:10:13 -0600

    public route table added

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -67,3 +67,16 @@ resource "aws_internet_gateway" "igw" {
     Environment = var.env
   }
 }
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.env}-public-rt" ,
+    Environment = var.env
+  }
+}
